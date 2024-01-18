29119e29739edbdd3209286d2546ac500c3cb2cc
HichamZineb <hichamelamine@hotmail.com>
2024-01-18 15:20:05 +0000

    update 7-average_score.sql

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,21 +1,14 @@
 -- Create a stored procedure ComputeAverageScoreForUser
 DELIMITER //
-CREATE PROCEDURE ComputeAverageScoreForUser(
-    IN p_user_id INT
-)
+
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
+CREATE PROCEDURE ComputeAverageScoreForUser (IN user_id INT)
 BEGIN
-    DECLARE total_score FLOAT;
-    DECLARE total_projects INT;
+	UPDATE users
+	SET
+	average_score = (SELECT AVG(score) FROM corrections WHERE corrections.user_id = user_id)
+	WHERE id = user_id;
 
-    -- Calculate total score and total number of projects for the user
-    SELECT SUM(score) INTO total_score, COUNT(DISTINCT project_id) INTO total_projects
-    FROM corrections
-    WHERE user_id = p_user_id;
+END $$
 
-    -- Calculate and update the average score for the user
-    UPDATE users
-    SET average_score = IF(total_projects > 0, total_score / total_projects, 0)
-    WHERE id = p_user_id;
-END;
-//
 DELIMITER ;
