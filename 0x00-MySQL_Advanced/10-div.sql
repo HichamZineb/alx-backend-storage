20cea65368c7aaacb69f976df29f33004e08096b
HichamZineb <hichamelamine@hotmail.com>
2024-01-18 15:38:33 +0000

    update 10-div.sql

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -1,13 +1,14 @@
-DELIMITER $$
-
-CREATE FUNCTION SafeDiv(a INT, b INT)
-RETURNS DECIMAL(10, 6)
+-- Function: SafeDiv
+-- Description: Safely divides the first number by the second number or returns 0 if the second number is equal to 0.
+-- Parameters:
+--   a - INT: Numerator
+--   b - INT: Denominator
+-- Returns: DECIMAL(10, 6) - Result of the division or 0 if the denominator is 0
+CREATE FUNCTION SafeDiv(a INT, b INT) RETURNS DECIMAL(10, 6)
 BEGIN
     IF b = 0 THEN
         RETURN 0;
     ELSE
         RETURN a / b;
     END IF;
-END $$
-
-DELIMITER ;
+END;
