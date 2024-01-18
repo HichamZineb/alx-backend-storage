581e7deedaba639fcd987e9144ccecaf024304a2
HichamZineb <hichamelamine@hotmail.com>
2024-01-18 13:42:45 +0000

    update 3-glam_rock.sql

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -5,10 +5,7 @@
 -- List all bands with Glam rock as their main style, ranked by their longevity
 SELECT
     band_name,
-    IFNULL(
-        IF(split = 0, 0, formed + split),
-        IF(splitted_formed = 0, 0, 2022 - splitted_formed)
-    ) AS lifespan
+    COALESCE(split, 2022) - formed as lifespan
 FROM
     metal_bands
 WHERE
