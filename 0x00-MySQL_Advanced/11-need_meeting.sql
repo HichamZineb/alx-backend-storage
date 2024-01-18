3bb83fee8fe8c339b0cfa56ee120e33af64d3043
HichamZineb <hichamelamine@hotmail.com>
2024-01-18 15:53:24 +0000

    update 11-need_meeting.sql

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -1,6 +1,7 @@
 -- Create the view need_meeting
+
 CREATE VIEW need_meeting AS
 SELECT name
 FROM students
 WHERE score < 80
-AND (students.last_meeting IS NULL OR students.last_meeting < DATE_ADD(NOW(), INTERVAL -1 MONTH);
+AND (students.last_meeting IS NULL OR students.last_meeting < DATE_ADD(NOW(), INTERVAL -1 MONTH));
