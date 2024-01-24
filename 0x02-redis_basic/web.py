b72a843eaba40d703abc5957c49d13ff294a4025
HichamZineb <hichamelamine@hotmail.com>
2024-01-24 15:38:25 +0000

    update web.py

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -2,48 +2,41 @@
 """
 Web Cache Module
 
-Defines the WebCache class for caching and tracking accessed URLs.
+Defines a decorator to cache and track accessed URLs.
 """
 
 import requests
 import redis
-from typing import Optional
+from typing import Callable
+
+# Redis client instance
+redis_client = redis.Redis()
 
 
-class WebCache:
+def cache_and_track(fn: Callable) -> Callable:
     """
-    WebCache class for caching and tracking accessed URLs.
+    Decorator function to cache and track accessed URLs.
 
-    Attributes:
-        _redis: Redis client instance.
+    Args:
+        fn (Callable): The function to be decorated.
+
+    Returns:
+        Callable: Decorated function.
     """
-
-    def __init__(self):
-        """Initializes the WebCache instance with a Redis client."""
-        self._redis = redis.Redis()
-
-    def get_page(self, url: str) -> str:
-        """
-        Fetches the content of a URL, tracks access count
-        and caches the content with expiration.
-
-        Args:
-            url (str): URL to fetch content from.
-
-        Returns:
-            str: Content of the URL.
-        """
+    def wrapper(url: str) -> str:
         count_key, content_key = f"count:{url}", f"content:{url}"
 
-        cached_content = self._redis.get(content_key)
+        cached_content = redis_client.get(content_key)
         if cached_content:
-            self._redis.incr(count_key)
+            redis_client.incr(count_key)
             return cached_content.decode("utf-8")
 
         response = requests.get(url)
         content = response.text
 
-        self._redis.setex(content_key, 10, content)
-        self._redis.incr(count_key)
+        redis_client.setex(content_key, 10, content)
+        redis_client.incr(count_key)
 
         return content
+
+    return wrapper
